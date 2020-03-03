a602c917f18cc0f846153e5851eb2989f1e7cacb
mbrzez <info@brzezins.pl>
2020-03-03 09:33:47 +0100

    Dummy implementation of getRomanLiteral method

--- src/main/java/pl/brzezins/converter/Arabic2RomanConverter.java
+++ src/main/java/pl/brzezins/converter/Arabic2RomanConverter.java
@@ -4,22 +4,25 @@ import java.util.Stack;
 
 public class Arabic2RomanConverter implements NumberConverter<String, Integer> {
     public String convert(Integer number) {
-        int n = number;
-        String result = new String();
-        Stack<Integer> numbers = new Stack<>();
-
-        do {
-            numbers.add(n % 10);
-        } while ((n = n / 10) != 0);
-
-        while (numbers.size() != 0) {
-            result += getRomanForDecimal(numbers.size() * numbers.pop());
+        if (number < 0 || number > 4000) {
+            throw new IllegalArgumentException("The number should be between 1 and 4000");
         }
 
-        return result;
+        StringBuilder result = new StringBuilder();
+
+        // Process decimals from back to the top
+        for (int i = 0; i < 4; i++) {
+            result.append(getRomanLiteral(number % 10, i));
+            number /= 10;
+        }
+
+        // Then reverse number
+        return result.reverse().toString();
     }
 
-    private String getRomanForDecimal(int decimal) {
+    private String getRomanLiteral(int digit, int position) {
+        //TODO
+
         return "X";
     }
 }
